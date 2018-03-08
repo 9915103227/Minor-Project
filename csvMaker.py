4ededa366a1712e06c09f1c69b5ba87bfc684ae3
Pranav Saxena <psaxena130@gmail.com>
2018-03-08 23:24:23 +0530

    Update csvMaker.py

--- csvMaker.py
+++ csvMaker.py
@@ -16,18 +16,21 @@ l= len(data);
 print l
 with open('bitcoinTweet.csv', 'wb') as myfile:
 	for i in range(l):
-		list=[];
-		#list.append(data[i]['volume'])
-		#list.append(data[i]['bid']);
-		list.append(data[i]['created_at'])
-		list.append(data[i]['text'])
-		list.append(data[i]['user']['description'])
-		list.append(data[i]['user']['protected'])
-		list.append(data[i]['user']['verified'])
-		list.append(data[i]['user']['followers_count'])
-		list.append(data[i]['user']['friends_count'])
-		list.append(data[i]['user']['favourites_count'])
-		list.append(data[i]['user']['statuses_count'])
-		wr = csv.writer(myfile, quoting=csv.QUOTE_ALL);
-		wr.writerow(list);
+		try:
+			list=[];
+			#list.append(data[i]['volume'])
+			#list.append(data[i]['bid']);
+			list.append(data[i]['created_at'])
+			list.append(data[i]['text'])
+			list.append(data[i]['user']['description'])
+			list.append(data[i]['user']['protected'])
+			list.append(data[i]['user']['verified'])
+			list.append(data[i]['user']['followers_count'])
+			list.append(data[i]['user']['friends_count'])
+			list.append(data[i]['user']['favourites_count'])
+			list.append(data[i]['user']['statuses_count'])
+			wr = csv.writer(myfile, quoting=csv.QUOTE_ALL);
+			wr.writerow(list);
+		except:
+			continue
 		
