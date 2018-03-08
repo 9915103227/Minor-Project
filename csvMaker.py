a32000a6cdc7e00f6f1aff97a51058ca7c4e1def
Pranav Saxena <psaxena130@gmail.com>
2018-03-09 00:36:35 +0530

    Field for sentiment polarity

--- csvMaker.py
+++ csvMaker.py
@@ -1,6 +1,7 @@
 import json
 import demjson
 from pprint import pprint
+from textblob import TextBlob
 #import csv
 import unicodecsv as csv
 #json_data=open("./sample.json").read()
@@ -8,12 +9,12 @@ import unicodecsv as csv
 data = json.load(open('bitcoinTweet.json','r'))
 #data=demjson.encode(open('bitcoin_price.json'))
 
-pprint(data)
+#pprint(data)
 
 #print data[0]['volume'];
 #print data[1]['volume'];
 l= len(data);
-print l
+print(l)
 with open('bitcoinTweet.csv', 'wb') as myfile:
 	for i in range(l):
 		try:
@@ -21,7 +22,10 @@ with open('bitcoinTweet.csv', 'wb') as myfile:
 			#list.append(data[i]['volume'])
 			#list.append(data[i]['bid']);
 			list.append(data[i]['created_at'])
-			list.append(data[i]['text'])
+			str=TextBlob(data[i]['text']);
+			polarity=str.sentiment.polarity
+			list.append(polarity)
+			#list.append(data[i]['text'])
 			list.append(data[i]['user']['description'])
 			list.append(data[i]['user']['protected'])
 			list.append(data[i]['user']['verified'])
@@ -29,6 +33,7 @@ with open('bitcoinTweet.csv', 'wb') as myfile:
 			list.append(data[i]['user']['friends_count'])
 			list.append(data[i]['user']['favourites_count'])
 			list.append(data[i]['user']['statuses_count'])
+			print(list)
 			wr = csv.writer(myfile, quoting=csv.QUOTE_ALL);
 			wr.writerow(list);
 		except:
