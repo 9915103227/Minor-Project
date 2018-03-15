6e758a861a5448d9fa64c752257541c8c1ad22d7
Pranav Saxena <psaxena130@gmail.com>
2018-03-15 12:24:22 +0530

    Update csvMaker.py

--- csvMaker.py
+++ csvMaker.py
@@ -6,18 +6,23 @@ from textblob import TextBlob
 import unicodecsv as csv
 #json_data=open("./sample.json").read()
 
-data = json.load(open('bitcoinTweet.json','r'))
+data = json.load(open('1.json','r'))
 #data=demjson.encode(open('bitcoin_price.json'))
 
 #pprint(data)
 
 #print data[0]['volume'];
 #print data[1]['volume'];
-l= len(data);
-print(l)
+i= len(data);
+print(i)
+i=i-1
 with open('bitcoinTweet.csv', 'wb') as myfile:
-	for i in range(l):
-		try:
+	while(i>=0):
+		#try:
+			x=data[i]['metadata']['iso_language_code']
+			if(x!="en"):
+				i=i-1
+				continue
 			list=[];
 			#list.append(data[i]['volume'])
 			#list.append(data[i]['bid']);
@@ -26,16 +31,19 @@ with open('bitcoinTweet.csv', 'wb') as myfile:
 			polarity=str.sentiment.polarity
 			list.append(polarity)
 			#list.append(data[i]['text'])
-			list.append(data[i]['user']['description'])
+			#list.append(data[i]['user']['description'])
 			list.append(data[i]['user']['protected'])
 			list.append(data[i]['user']['verified'])
 			list.append(data[i]['user']['followers_count'])
 			list.append(data[i]['user']['friends_count'])
 			list.append(data[i]['user']['favourites_count'])
 			list.append(data[i]['user']['statuses_count'])
+			list.append(data[i]['retweet_count'])
+
 			print(list)
 			wr = csv.writer(myfile, quoting=csv.QUOTE_ALL);
 			wr.writerow(list);
-		except:
-			continue
-		
+			i=i-1
+			print(list)
+		#except:
+		#	continue
